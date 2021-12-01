5f2e244f0927a49a8b037ffff8de3b97f4fc3dcb
mis320 <62322924+mis320@users.noreply.github.com>
2021-12-01 16:03:16 +0800

    Update nodes.js

--- swap2/nodes.js
+++ swap2/nodes.js
@@ -21,7 +21,7 @@ const nodeUrls = [
 
 
 const SWAP_TOKEN = '0xD71A8Aed37541983715fe35C9A08D4665b537d1A'
-const GET_INFO_TOKEN = '0x236af7E305A2b35d8D24ad57eBb446D991e208fc'
+const GET_INFO_TOKEN = '0x8A0dc6be1963684D1C122Ca237052Fb495A97c48'
 const encrypt = 'whdcay8324291kjscbisfg8ginsid$@535612'
 
 function web3Nodespush() {
