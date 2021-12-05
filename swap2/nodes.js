9ea7c56251f9a161f7282c09653a68965b3465ca
mis320 <62322924+mis320@users.noreply.github.com>
2021-12-05 19:40:48 +0800

    Update nodes.js

--- swap2/nodes.js
+++ swap2/nodes.js
@@ -1,6 +1,7 @@
 let web3Nodes = []
 let web3Methods = []
 let getInfoMethods = []
+let tempNodes =[]
 const nodeUrls = [
     'https://bsc-dataseed4.binance.org',
     'https://bsc-dataseed3.binance.org',
@@ -18,13 +19,21 @@ const nodeUrls = [
     'http://bsc.mytokenpocket.vip'
 ]
 
-
+const swapInfo ={
+    1:"pancakeSwap(薄饼)",
+    2:"bakerySwap(面包)",
+    3:"babySwap(北鼻)",
+    4:"apeswap(猩猩)",
+    5:"",
+    6:"",
+    7:"",
+    8:""
+}
 
 const SWAP_TOKEN = '0xD71A8Aed37541983715fe35C9A08D4665b537d1A'
-const GET_INFO_TOKEN = '0x8A0dc6be1963684D1C122Ca237052Fb495A97c48'
+const GET_INFO_TOKEN = '0x236af7E305A2b35d8D24ad57eBb446D991e208fc'
 const encrypt = 'whdcay8324291kjscbisfg8ginsid$@535612'
-
-function web3Nodespush() {
+function web3NodesPush() {
     for (let index = 0; index < nodeUrls.length; index++) {
         const url = nodeUrls[index];
         let web3 = newWeb3(url)
@@ -32,18 +41,21 @@ function web3Nodespush() {
         getInfoMethods.push(new web3.eth.Contract(GET_INFO_ABI, GET_INFO_TOKEN).methods)
         web3Methods.push(new web3.eth.Contract(gan_si_gou_zhuang_ABI, SWAP_TOKEN).methods)
     }
-
-    console.log('节点', web3Nodes);
+    console.log('Nodes', web3Nodes);
 }
 
-
-
+function tempNodesPush(abi,token) {
+    for (let index = 0; index < nodeUrls.length; index++) {
+        const url = nodeUrls[index];
+        let web3 = newWeb3(url)
+        tempNodes.push(new web3.eth.Contract(abi, token).methods)
+    }
+    console.log('tempNodes', web3Nodes);
+}
 function newWeb3(url) {
     let web3 = new Web3(url)
     return web3
 }
-
-
 
 function accountWalletAdd(key) {
     for (let index = 0; index < web3Nodes.length; index++) {
@@ -56,8 +68,6 @@ function accountWalletAdd(key) {
     }
 }
 
-
-
 function accountWalletClear() {
     for (let index = 0; index < web3Nodes.length; index++) {
         let web3 = web3Nodes[index];
@@ -69,7 +79,6 @@ function web3Router(k) {
     return web3Nodes[k % 14]
 }
 
-
 function web3NodeSwap(k) {
     return web3Methods[k % 14]
 }
@@ -78,4 +87,4 @@ function getInfoRouter(k) {
     return getInfoMethods[k % 14]
 }
 //web3Nodespush()
-web3Nodespush()
+web3NodesPush()
