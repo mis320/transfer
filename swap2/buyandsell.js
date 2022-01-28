abe524de8c0c113fa48510c207c7b30ca5f489ba
mis320 <62322924+mis320@users.noreply.github.com>
2022-01-28 18:08:58 +0800

    Update buyandsell.js

--- swap2/buyandsell.js
+++ swap2/buyandsell.js
@@ -1,36 +1,35 @@
 const weth = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'
 const usdt = '0x55d398326f99059ff775485246999027b3197955'
 const busdt = '0xe9e7cea3dedca5984780bafc599bd69add087d56'
+const ball = '0x155da5619a9bea25f0f57f15297a8dba6bb28198'
+const fist = ''
 
 function getPaths(contract) {
-
-
     let ETHpathBuy = []
     ETHpathBuy.push(weth)
     ETHpathBuy.push(contract)
-
     let USDpathBuy = []
     USDpathBuy.push(weth)
     USDpathBuy.push(usdt)
     USDpathBuy.push(contract)
-
-
     let BUSDpathBuy = []
     BUSDpathBuy.push(weth)
     BUSDpathBuy.push(busdt)
     BUSDpathBuy.push(contract)
+    let BallpathBuy = []
+    BallpathBuy.push(weth)
+    BallpathBuy.push(ball)
+    BallpathBuy.push(contract)
 
     let pathsBuy = []
     pathsBuy.push(ETHpathBuy)
     pathsBuy.push(USDpathBuy)
     pathsBuy.push(BUSDpathBuy)
-
-
-
+    pathsBuy.push(BallpathBuy)
 
 
     let ETHpathSell = []
-    
+
     ETHpathSell.push(contract)
     ETHpathSell.push(weth)
 
@@ -38,22 +37,29 @@ function getPaths(contract) {
     USDpathSell.push(contract)
     USDpathSell.push(usdt)
     USDpathSell.push(weth)
-    
+
 
 
     let BUSDpathSell = []
     BUSDpathSell.push(contract)
     BUSDpathSell.push(busdt)
     BUSDpathSell.push(weth)
+    let BallpathSell = []
+    BallpathSell.push(contract)
+    BallpathSell.push(ball)
+    BallpathSell.push(weth)
 
 
     let pathsSell = []
     pathsSell.push(ETHpathSell)
     pathsSell.push(USDpathSell)
     pathsSell.push(BUSDpathSell)
+    pathsSell.push(BallpathSell)
 
 
-    return {"buy":pathsBuy,'sell':pathsSell}
+    return {
+        "buy": pathsBuy,
+        'sell': pathsSell
+    }
 
 }
-
