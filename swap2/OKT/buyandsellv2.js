667bb038bc4338535250b10aa069da9b662f01e8
mis320 <62322924+mis320@users.noreply.github.com>
2022-04-07 15:59:36 +0800

    Update buyandsellv2.js

--- swap2/OKT/buyandsellv2.js
+++ swap2/OKT/buyandsellv2.js
@@ -1,5 +1,5 @@
 const weth = '0x8F8526dbfd6E38E3D8307702cA8469Bae6C56C15'
-const usdt = '0x55d398326f99059ff775485246999027b3197955'
+const usdt = '0x382bb369d343125bfb2117af9c149795c6c65c50'
 const busdt = '0xdcac52e001f5bd413aa6ea83956438f29098166b'
 /* const ball = '0x155da5619a9bea25f0f57f15297a8dba6bb28198'
 const fist = '0xc9882def23bc42d53895b8361d0b1edc7570bc6a'
@@ -17,9 +17,8 @@ const fst_weth = '0x0efb5fd2402a0967b92551d6af54de148504a115' */
 
 function getpathsIndex(contract, index) {
     index
-   let WETH = weth
-    /* 
-    if (index == 6) {
+    let WETH = weth
+   /*  if (index == 6) {
         WETH = fst_weth
     } */
 
